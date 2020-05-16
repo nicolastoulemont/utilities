37b204278384ec602a40a363733ccb9b28a68917
Nicolas Toulemont <n.toulemont@gmail.com>
2020-05-16 17:40:42 +0200

    added more methods on the stack class

--- src/stack.ts
+++ src/stack.ts
@@ -13,7 +13,7 @@ export class Stack<T> {
 
   pop() {
     const lastIndex = this.length - 1;
-    if (lastIndex < 0) return false;
+    if (lastIndex < 0) return null;
     const lastValue = this.storage[lastIndex];
     delete this.storage[lastIndex];
     this.length--;
@@ -23,5 +23,55 @@ export class Stack<T> {
   peek() {
     const lastIndex = this.length - 1;
     return this.storage[lastIndex];
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  print() {
+    if (this.storage === {}) {
+      console.log('Empty stack');
+    } else {
+      for (const key in this.storage) {
+        console.log(`${key} : ${this.storage[key]}`);
+      }
+    }
+  }
+
+  insertAtBottom(stack: Stack<T>, item: T) {
+    if (stack.isEmpty()) {
+      stack.push(item);
+    } else {
+      const temp = stack.pop();
+      this.insertAtBottom(stack, item);
+      stack.push(temp as T);
+    }
+  }
+
+  reverse(stack: Stack<T>) {
+    if (!stack.isEmpty()) {
+      const temp = stack.pop();
+      this.reverse(stack);
+      this.insertAtBottom(stack, temp as T);
+    }
+  }
+
+  sort(stack: Stack<T>) {
+    if (!stack.isEmpty()) {
+      const temp = stack.pop();
+      this.sort(stack);
+      this.sortedInsert(stack, temp as T);
+    }
+  }
+
+  sortedInsert(stack: Stack<T>, item: T) {
+    if (stack.isEmpty() || item > stack.peek()) {
+      stack.push(item);
+    } else {
+      const temp = stack.pop();
+      this.sortedInsert(stack, item);
+      stack.push(temp as T);
+    }
   }
 }
--- test/stack.test.ts
+++ test/stack.test.ts
@@ -12,7 +12,40 @@ describe('Testing Stack', () => {
     const stack = new Stack();
     const item = 'item';
     stack.push(item);
-    const lastItem = stack.pop();
-    expect(lastItem).toStrictEqual(item);
+    expect(stack.pop()).toStrictEqual(item);
+  });
+  it('Stack isEmpty', () => {
+    const stack = new Stack();
+    const isEmpty = stack.isEmpty();
+    expect(isEmpty).toBe(true);
+    stack.push('item');
+    expect(stack.isEmpty()).toBe(false);
+  });
+  it('Insert at bottom', () => {
+    const stack = new Stack();
+    stack.push(1);
+    stack.push(2);
+    stack.insertAtBottom(stack, 3);
+    expect(stack.pop()).toBe(2);
+  });
+  it('Reverse stack', () => {
+    const stack = new Stack();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.reverse(stack);
+    expect(stack.pop()).toBe(1);
+    expect(stack.pop()).toBe(2);
+    expect(stack.pop()).toBe(3);
+  });
+  it('Sort stack', () => {
+    const stack = new Stack();
+    stack.push(1);
+    stack.push(3);
+    stack.push(2);
+    stack.sort(stack);
+    expect(stack.pop()).toBe(3);
+    expect(stack.pop()).toBe(2);
+    expect(stack.pop()).toBe(1);
   });
 });
